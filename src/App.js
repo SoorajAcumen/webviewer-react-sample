58777fcb53eac33cff924f7c82ace01f4de16c0a
SoorajAcumen <sooraj.acumenvelocity@gmail.com>
2024-11-11 15:13:27 +0530

    PDF DOCX url option added

--- src/App.js
+++ src/App.js
@@ -4,42 +4,29 @@ import './App.css';
 
 const App = () => {
   const viewer = useRef(null);
+  const url = 'https://storage.googleapis.com/moksh-dev/word-doc.docx'
+  // const url = 'https://icseindia.org/document/sample.pdf'
 
-  // if using a class, equivalent of componentDidMount 
   useEffect(() => {
-    // If you prefer to use the Iframe implementation, you can replace this line with: WebViewer.Iframe(...)
-    WebViewer.WebComponent(
+    WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-        licenseKey: 'your_license_key',  // sign up to get a free trial key at https://dev.apryse.com
+        licenseKey: 'demo:1731305902335:7efef1130300000000ecd5e4619a6c4248b80b08e908e02111ef1543ca', // Demo license key for WebViewer
+        initialDoc: `https://cors-anywhere.herokuapp.com/${url}`,
+        enableOfficeEditing: true,
       },
-      viewer.current,
+      viewer.current
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
-
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+      instance.UI.enableFeatures([instance.UI.Feature.ContentEdit]);
+      // instance.UI.setToolbarGroup(instance.UI.ToolbarGroup.EDIT_TEXT);
+    }).catch((error) => {
+      console.error('Error initializing WebViewer:', error);
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
+      <div className="webviewer" ref={viewer} style={{ height: '100vh' }}></div>
     </div>
   );
 };
